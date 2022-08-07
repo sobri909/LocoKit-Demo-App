c1df66f94c9837cc3dcaca9d226280eede3191ca
Matt <matt@bigpaua.com>
2022-08-07 16:59:53 +0700

    Initial Commit

---
+++ LocoKit Demo App/LocoKit_Demo_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocoKit_Demo_AppApp.swift
+//  LocoKit Demo App
+//
+//  Created by Matt Greenfield on 7/8/22.
+//
+
+import SwiftUI
+
+@main
+struct LocoKit_Demo_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
